114065ac63c94a7727d3e8cc52b88957735ac4f6
Abhishek Kumar <118982365+abhikumar0670@users.noreply.github.com>
2025-09-07 02:14:16 +0530

    Time: 11 ms (100.00%) | Memory: 101.3 MB (38.78%) - LeetSync

--- 3744-minimum-operations-to-make-array-elements-zero/minimum-operations-to-make-array-elements-zero.java
+++ 3744-minimum-operations-to-make-array-elements-zero/minimum-operations-to-make-array-elements-zero.java
@@ -1,34 +1,25 @@
 class Solution {
-    private long sumStepsUpTo(long n) {
-        if (n <= 0) return 0L;
-        long sum = 0L;
-        long pow = 1L;
-        int k = 1;
-        while (pow <= n) {
-            long nextPow;
-            if (pow > Long.MAX_VALUE / 4) nextPow = Long.MAX_VALUE;
-            else nextPow = pow * 4L;
-            long upper = nextPow - 1L;
-            long hi = Math.min(n, upper);
-            long cnt = hi - pow + 1L;
-            if (cnt > 0) sum += cnt * (long) k;
-            if (nextPow == Long.MAX_VALUE) break;
-            pow = nextPow;
-            k++;
-        }
-        return sum;
-    }
-
     public long minOperations(int[][] queries) {
-        long total = 0L;
-        for (int[] q : queries) {
-            long l = q[0];
-            long r = q[1];
-            long sumSteps = sumStepsUpTo(r) - sumStepsUpTo(l - 1);
-            long ops = (sumSteps + 1) / 2;
-            total += ops;
+        long res = 0;
+        for (int[] query: queries) res += minOperations(query);
+        return res;
+    }
+    public static long minOperations(int[] query) {
+        long res0 = 0;
+        long x = 1;
+        while (x < query[0]) {
+            res0 ++;
+            x *= 4;
         }
-        return total;
+        long res1 = res0;
+        long res = 0;
+        long prev = query[0];
+        while (x <= query[1] * 4L) {
+            res += res1 * (Math.min(x, query[1] + 1) - prev);
+            prev = x;
+            res1 ++;
+            x *= 4;
+        }
+        return (res + 1) / 2;
     }
 }
-
